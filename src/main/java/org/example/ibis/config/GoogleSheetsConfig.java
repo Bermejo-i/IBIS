ad9a459b79bd87e242b364c29a5b1dea677ee609
Bermejo-i <israelbermejo.1@gmail.com>
2025-06-11 22:03:35 -0500

    Update GoogleSheetsConfig.java

--- src/main/java/org/example/ibis/config/GoogleSheetsConfig.java
+++ src/main/java/org/example/ibis/config/GoogleSheetsConfig.java
@@ -2,26 +2,20 @@ package org.example.ibis.config;
 
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.sheets.v4.Sheets;
-import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.auth.http.HttpCredentialsAdapter;
 import com.google.auth.oauth2.GoogleCredentials;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.security.GeneralSecurityException;
 import java.util.Collections;
 
 @Configuration
 public class GoogleSheetsConfig {
 
     @Bean
-public Sheets sheetsService() throws Exception {
-        // Cargar desde classpath (funciona en local y en .jar)
+    public Sheets sheetsService() throws Exception {
         InputStream credentialsStream = getClass().getClassLoader().getResourceAsStream("credentials.json");
 
         if (credentialsStream == null) {
@@ -34,7 +28,7 @@ public Sheets sheetsService() throws Exception {
 
         return new Sheets.Builder(
                 GoogleNetHttpTransport.newTrustedTransport(),
-                JacksonFactory.getDefaultInstance(),
+                GsonFactory.getDefaultInstance(),
                 new HttpCredentialsAdapter(credentials)
         ).setApplicationName("IBIS").build();
     }
